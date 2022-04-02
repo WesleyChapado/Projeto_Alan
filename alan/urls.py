17645348db7d3d07cc9fdc85300c535acf9bc235
WesleyChapado <73513545+WesleyChapado@users.noreply.github.com>
2022-04-02 16:27:50 -0300

    Criando view, model e serializer de Organization

--- alan/urls.py
+++ alan/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from corev1.views import UserView
+from corev1.views import OrganizationView, UserView
 
 router = routers.DefaultRouter()
 
 # registra suas outras rotas
-router.register('register', UserView, basename = 'User')
+router.register('user', UserView, basename = 'User')
+router.register('organization', OrganizationView, basename = 'Organization')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1.0/', include(router.urls))
+    path('v1.0/register/', include(router.urls))
 ]
--- corev1/serializer.py
+++ corev1/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from corev1.models import UserModel
+from corev1.models import UserModel, OrganizationModel
 
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = UserModel
+		exclude = []
+
+class OrganizationSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = OrganizationModel
 		exclude = []
